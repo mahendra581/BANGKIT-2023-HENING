a29fd2217510104d5369302a2742547c1ca59e4a
masitaaz <ohmasita@gmail.com>
2023-06-12 12:49:01 +0700

    API Improvement adding course

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const v1Routes = require("./src/v1/routes/apiRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
